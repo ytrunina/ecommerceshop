f7e900ff1139204a3ee9ea311dea57fded5c49c8
yelena <yt21c@fsu.edu>
2024-01-24 17:49:35 -0500

    5.28

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import {
     Route,
     RouterProvider
 } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -28,7 +30,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store={store}>
         <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>
 );
 
