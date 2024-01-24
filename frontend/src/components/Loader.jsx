692fbbefdbd148f6b1f02be598a5bfba0c2282b0
yelena <yt21c@fsu.edu>
2024-01-24 18:29:07 -0500

    5.31

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import  { Spinner } from 'react-bootstrap';
+
+const Loader = () => {
+  return (
+    <div>
+        <Spinner animation='border' 
+        role='status' 
+        style={{ width: '100px', height: '100px', margin: 'auto', display: 'block'}}
+        ></Spinner>
+    </div>
+  )
+}
+
+export default Loader
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,14 +2,15 @@
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
-
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
     const { data: products, isLoading, error } = useGetProductsQuery();
     return (
         <>
-        {isLoading ? (<h2>Loading...</h2>) : error ? (
-            <div>{ error?.data?.message || error.error}</div> 
+        {isLoading ? <Loader /> : error ? (
+            <Message variant= 'danger'>{ error?.data?.message || error.error}</Message> 
         ) : (
             <>
             <h1>Latest Products</h1>
