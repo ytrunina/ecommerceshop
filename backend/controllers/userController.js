68e35623f4996cea4f5fe7b3760a9b7c2c8b60a0
yelena <yt21c@fsu.edu>
2024-01-28 18:15:24 -0500

    7.44 logout & clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,7 +51,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send("logout");
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: "User logged out" });
 });
 
 // @desc   Get user profile
