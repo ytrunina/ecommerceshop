dcefc32c1b3874a4a7126848decff30863d287e1
yelena <yt21c@fsu.edu>
2024-01-28 17:42:13 -0500

    7.40 - User email & pw validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,20 @@ import User from "../models/userModel.js";
 // @access  Public
 
 const authUser = asyncHandler(async (req, res) => {
-    res.send("auth user");
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null,
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+    res.send({ email, password });
 });
 
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
     name: {
@@ -23,5 +24,9 @@ const userSchema = mongoose.Schema({
         default: false
     }
 }, { timestamps: true });
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model('User', userSchema);
