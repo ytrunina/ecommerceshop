c5a462aff43151ba02dc4f32a6ba698a39d0d5fe
yelena <yt21c@fsu.edu>
2024-02-10 22:32:36 -0500

    bug fix for PrivateRoute.jsx

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,10 +1,8 @@
-import {Outlet, Navigate} from 'react-router-dom';
-import { UseSelector, useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const PrivateRoute = ({ }) => {
-    const userInfo = useSelector((state) => state.user);
-    
-    return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
-}
-
+const PrivateRoute = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
+};
 export default PrivateRoute;
